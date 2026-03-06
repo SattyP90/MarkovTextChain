f6760f02dfebe54017f5307e910e6593f12d6efe
Sattyaj Paul <sp972@kent.ac.uk>
2026-03-06 12:37:20 +0000

    Initial Basic markov chain.
    -simple trigram markov for more accurate probabilities on consecutive words.
    - using lord of the flies as the first sample text.
    - generates clear sentence some of the make sense.

---
+++ markovChain.py
@@ -0,0 +1,58 @@
+import random
+import re
+
+# Read text file
+with open("LOTF.txt", "r", encoding="utf-8") as file:
+    text = file.read()
+
+# Convert to lowercase
+text = text.lower()
+
+# Split text into sentences using '.'
+sentences = text.split(".")
+
+trigram_table = {}
+
+for sentence in sentences:
+    
+    # Remove punctuation except spaces
+    sentence = re.sub(r'[^a-z\s]', '', sentence)
+
+    words = sentence.split()
+
+    # Skip short sentences
+    if len(words) < 3:
+        continue
+
+    # Build trigram relationships
+    for i in range(len(words) - 2):
+        w1 = words[i]
+        w2 = words[i + 1]
+        w3 = words[i + 2]
+
+        key = (w1, w2)
+
+        if key not in trigram_table:
+            trigram_table[key] = []
+
+        trigram_table[key].append(w3)
+
+# Generate a 10 word sentence
+start = random.choice(list(trigram_table.keys()))
+w1, w2 = start
+
+sentence = [w1, w2]
+
+for _ in range(8):  # already have 2 words
+    key = (w1, w2)
+
+    if key not in trigram_table:
+        break
+
+    next_word = random.choice(trigram_table[key])
+    sentence.append(next_word)
+
+    w1, w2 = w2, next_word
+
+
+print(" ".join(sentence))
